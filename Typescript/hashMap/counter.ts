fb9e3a89274fdc6013bb2a7d86b0db4ee5d8658d
DanielAdek <maildaniel.me1@gmail.com>
2025-03-12 20:32:13 +0100

    working on hash map in typescript

---
+++ Typescript/hashMap/counter.ts
@@ -0,0 +1,11 @@
+/**
+ Problem 1: Counting Frequency of Elements
+  Problem:
+    Given an array of numbers, count the frequency of each number.
+ */
+
+const frequencyCounter = (arr: number[]): { [key: number]: number } => {
+  const frequencyCounter: { [key: number]: number } = {};
+  
+  return frequencyCounter;
+}
